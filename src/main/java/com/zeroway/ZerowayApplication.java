74aed79b3295ab925bccff7e85b0d813c07b13d9
jihyehann <haneh0714@gmail.com>
2022-06-30 23:46:07 +0900

    first commit

---
+++ src/main/java/com/zeroway/ZerowayApplication.java
@@ -0,0 +1,13 @@
+package com.zeroway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerowayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerowayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeroway/ZerowayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerowayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
