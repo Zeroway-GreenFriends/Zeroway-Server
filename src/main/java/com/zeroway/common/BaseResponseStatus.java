9894bedd74a38dd69b9d1e071a3b8be42322ff02
jihyehann <haneh0714@gmail.com>
2022-07-01 11:24:33 +0900

    feat: common 패키지 추가
    - BaseException, BaseResponse, BaseResponseStatus

---
+++ src/main/java/com/zeroway/common/BaseResponseStatus.java
@@ -0,0 +1,50 @@
+package com.zeroway.common;
+
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+
+    /**
+     * 2000 : Request 오류
+     */
+    // Common
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+
+
+    /**
+     * 3000 : Response 오류
+     */
+    // Common
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
+
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다.");
+
+
+    // 5000 : 필요시 만들어서 쓰세요
+    // 6000 : 필요시 만들어서 쓰세요
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
