752331e79ef2eb44f738fab4be5694de56fa4afc
Han Jihye <haneh0714@gmail.com>
2022-07-21 11:48:58 +0900

    refactor: 엔티티 수정 및 추가
    - User, PostImage, Challenge, Term, Tip 수정
    - Level, PostLike, Store, StoreReview 추가

--- src/main/java/com/zeroway/user/entity/User.java
+++ src/main/java/com/zeroway/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.zeroway.user.entity;
 
+import com.zeroway.challenge.entity.Level;
 import com.zeroway.common.BaseEntity;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -17,21 +18,30 @@ public class User extends BaseEntity {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String nickname;
 
-    // 달성한 챌린지의 개수
-    @ColumnDefault("0")
-    @Builder.Default
-    private Integer challengeCount = 0;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private ProviderType provider;
+
+    private String profileImgUrl;
+
+    private String refreshToken;
 
     // 캐릭터 레벨
-    @ColumnDefault("1")
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "level_id")
+    private Level level;
+
+
+    // 경험치
+    @ColumnDefault("0")
     @Builder.Default
-    private Integer level = 1;
+    private Integer exp = 0;
 
     public User(String email, String nickname) {
         this.email = email;
--- src/main/java/com/zeroway/user/service/UserService.java
+++ src/main/java/com/zeroway/user/service/UserService.java
@@ -44,8 +44,8 @@ public class UserService {
                 .id(user.getId())
                 .email(user.getEmail())
                 .nickname(user.getNickname())
-                .challengeCount(user.getChallengeCount())
-                .level(user.getLevel())
+//                .challengeCount(user.getChallengeCount())
+                .level(user.getLevel().getId())
                 .build();
     }
 
@@ -83,8 +83,8 @@ public class UserService {
                 .id(user.getId())
                 .email(user.getEmail())
                 .nickname(user.getNickname())
-                .challengeCount(user.getChallengeCount())
-                .level(user.getLevel())
+//                .challengeCount(user.getChallengeCount())
+                .level(user.getLevel().getId())
                 .build();
     }
 }
