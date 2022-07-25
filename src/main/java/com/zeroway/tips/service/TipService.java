4f5bbe72086292422f5119f6f605c7b556b25424
Han Jihye <haneh0714@gmail.com>
2022-07-25 15:03:41 +0900

    feat: 오늘의 실천 tip 조회 API 추가
    - 랜덤으로 3개의 tip을 조회하는 api

--- src/main/java/com/zeroway/tips/service/TipService.java
+++ src/main/java/com/zeroway/tips/service/TipService.java
@@ -1,18 +1,16 @@
 package com.zeroway.tips.service;
 
 import com.zeroway.common.BaseException;
-import com.zeroway.common.BaseResponseStatus;
-import com.zeroway.tips.dto.AllTipRes;
-import com.zeroway.tips.entity.Term;
-import com.zeroway.tips.entity.Tip;
-import com.zeroway.tips.repository.TermRepository;
+import com.zeroway.tips.dto.TodayTipsRes;
 import com.zeroway.tips.repository.TipRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.zeroway.common.BaseResponseStatus.DATABASE_ERROR;
 
@@ -22,17 +20,27 @@ import static com.zeroway.common.BaseResponseStatus.DATABASE_ERROR;
 public class TipService {
 
     private final TipRepository tipRepository;
-    private final TermRepository termRepository;
 
     @Transactional(readOnly = true)
-    public AllTipRes getAllTips() throws BaseException {
-        try{
-            List<Tip> tips = tipRepository.findAll();
-            for (Tip tip : tips) {
-                System.out.println("tip = " + tip);
+    public List<TodayTipsRes> getRandomTips(long size) throws BaseException {
+        List<Long> idList = new ArrayList<>();
+
+        // 전체 tip 개수
+        long totalCount = tipRepository.count();
+
+        // 랜덤 id 생성
+        for (long i=0; i<size; i++) {
+            Long id = (long)(Math.random()*totalCount) + 1;
+            while (idList.contains(id)){
+                id = (long)(Math.random()*totalCount) + 1;
             }
-            List<Term> terms = termRepository.findAll();
-            return new AllTipRes(tips, terms);
+            idList.add(id);
+        }
+
+        try {
+            return tipRepository.findAllById(idList).stream()
+                    .map(tip -> new TodayTipsRes(tip.getTitle(), tip.getContent()))
+                    .collect(Collectors.toList());
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new BaseException(DATABASE_ERROR);
