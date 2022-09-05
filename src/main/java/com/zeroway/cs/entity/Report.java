323c0442cd29c9f7a90643f2be3650b2b6ffe60a
sojungpp <sophie2458@naver.com>
2022-09-05 14:08:30 +0900

    Feat: 신고(Report) entity 추가

---
+++ src/main/java/com/zeroway/cs/entity/Report.java
@@ -0,0 +1,47 @@
+package com.zeroway.cs.entity;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class Report {
+
+    @Id @GeneratedValue
+    @Column(name = "report_id")
+    private long id;
+
+    //신고한 사람
+    @Column(nullable = false)
+    private long userId;
+
+    //신고 카테고리
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryOfReport categoryOfReport;
+
+    //신고할 글 ID
+    @Column(nullable = false)
+    private long targetId;
+
+    //신고 유형
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TypeOfReport typeOfReport;
+
+    //신고 확인 여부
+    @ColumnDefault("false")
+    @Builder.Default
+    private boolean check;
+
+    @Builder
+    public Report(long userId, CategoryOfReport categoryOfReport, long targetId, TypeOfReport typeOfReport) {
+        this.userId = userId;
+        this.categoryOfReport = categoryOfReport;
+        this.targetId = targetId;
+        this.typeOfReport = typeOfReport;
+    }
+}
---
+++ src/main/java/com/zeroway/cs/entity/TypeOfReport.java
@@ -0,0 +1,25 @@
+package com.zeroway.cs.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum TypeOfReport {
+    ABUSE("욕설/비하"),
+    UNWHOLESOME("음란물/불건전한 만남 및 대화"),
+    SCAM("유출/사칭/사기"),
+    COMMERCIAL("상업적 광고 및 판매"),
+    PAPERING("낚시/도배");
+
+    private final String name;
+
+    public static TypeOfReport enumOf(String name) {
+        return Arrays.stream(TypeOfReport.values())
+                .filter(t -> t.getName().equals(name))
+                .findAny().orElse(null);
+    }
+
+}
