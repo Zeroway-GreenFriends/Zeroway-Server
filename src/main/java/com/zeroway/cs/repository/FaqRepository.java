a34d609aed55c681789d740f7d21ebfdeb459705
sojungpp <sophie2458@naver.com>
2022-09-22 00:48:20 +0900

    Chore: 고객센터 삭제 수정사항 반영

--- src/main/java/com/zeroway/cs/repository/FaqRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zeroway.cs.repository;
-
-import com.zeroway.cs.entity.FAQ;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaqRepository extends JpaRepository<FAQ, Long> {
-}
